77ab24b2ac89a685c2e7af69d1da22bdd9892c39
Sonicz7 <sonicgf93@gmail.com>
2026-04-04 15:34:59 +0200

    Add formateurRole to config.js

--- ticket bot/config/config.js
+++ ticket bot/config/config.js
@@ -2,6 +2,7 @@ export default {
     ticketCategory: "1487854009296621667",
     refusCategory: "1487854009502007320",
     staffRole: "1487854008042389540",
+    formateurRole: "1487854008042389541",
     ticketName: "ticket",
     ticketPanelChannel: "1487854009296621666",
     transcriptChannel: '1487854009502007323',
