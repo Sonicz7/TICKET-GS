265f3433a01caf1ce607c2502ef98dcb2b31abb3
Sonicz7 <sonicgf93@gmail.com>
2026-04-11 22:04:54 +0200

    Add files via upload

--- ticket bot/config/config.js
+++ ticket bot/config/config.js
@@ -3,6 +3,7 @@ export default {
     refusCategory: "1487854009502007320",
     staffRole: "1487854008042389540",
     formateurRole: "1487854008042389541",
+    viewerRole: "1487854008042389538",
     ticketName: "ticket",
     ticketPanelChannel: "1487854009296621666",
     transcriptChannel: '1487854009502007323',
